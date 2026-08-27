f96d7bcb075a835d9303821de3f7866c4666576b
Jadeandpaws <jadeandpaws@gmail.com>
2026-08-27 11:26:24 -0400

    Delete sitemap.ts

--- sitemap.ts
+++
@@ -1,2 +0,0 @@
-import type { MetadataRoute } from 'next';
-export default function sitemap(): MetadataRoute.Sitemap { const url = process.env.NEXT_PUBLIC_SITE_URL || 'https://jadeandpaws.com'; return [{ url, lastModified: new Date(), changeFrequency: 'monthly', priority: 1 }]; }

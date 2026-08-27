9c86d0c5ecb337eff70dca6e9b76b268fc0ed74e
Jadeandpaws <jadeandpaws@gmail.com>
2026-08-27 11:25:03 -0400

    Delete robots.ts

--- robots.ts
+++
@@ -1,2 +0,0 @@
-import type { MetadataRoute } from 'next';
-export default function robots(): MetadataRoute.Robots { const url = process.env.NEXT_PUBLIC_SITE_URL || 'https://jadeandpaws.com'; return { rules: { userAgent: '*', allow: '/' }, sitemap: `${url}/sitemap.xml` }; }
